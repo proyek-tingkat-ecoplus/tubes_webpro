3869043d4f118e714fcdb2356a701fc01c26fac0
Rafieaydin <rafie.aydin1@gmail.com>
2024-10-23 23:27:21 +0700

    add configure dashboard and user pages

---
+++ public/asset/admin/js/dashboardChart.js
@@ -0,0 +1,120 @@
+var color1 = '#264417';
+
+var options = {
+    chart: {
+        height: 350,
+        type: 'bar',
+        background: '#ffff',
+        foreground: '#333',
+    },
+    series: [{
+            name: 'Disubmit',
+            data: [18, 7, 15, 29, 18, 12, 9]
+        },
+        {
+            name: 'D    iapprove',
+            data: [13, 18, 9, 14, 5, 17, 15]
+        }
+    ],
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    },
+    // plotOptions: {
+    //     bar: {
+    //         horizontal: false,
+    //         columnWidth: '33%',
+    //         borderRadius: 12,
+    //         startingShape: 'rounded',
+    //         endingShape: 'rounded'
+    //     }
+    // },
+
+    fill: {
+        opacity: 1,
+        colors: [color1, '#437c4f'],
+    },
+    legend: {
+        show: true,
+        labels: {
+            colors: '#a1acb8',
+        },
+    },
+};
+const chart = new ApexCharts(document.querySelector('.bar_chart'), options);
+
+chart.render();
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Calendar({
+        id: '#calendar',
+        theme: 'basic',
+        calendarSize: 'small',
+        primaryColor: '#1a472a',
+        headerBackgroundColor: 'white',
+        eventsData: [{
+                id: 1,
+                name: "Community Meeting",
+                start: "2024-10-23",
+                end: "2024-10-23"
+            },
+            {
+                id: 2,
+                name: "Workshop on Empowerment",
+                start: "2024-10-25",
+                end: "2024-10-25"
+            },
+            {
+                id: 3,
+                name: "Empowerment Campaign",
+                start: "2024-10-24",
+                end: "2024-10-26"
+            }
+        ],
+        dateChanged: (currentDate, events) => {
+            // Handle date changes
+            showEventLabel(events);
+            // show label
+        },
+        monthChanged: (currentDate, events) => {
+            // Handle month changes
+            console.log('Current month:', currentDate);
+            console.log('Events:', events);
+        }
+    });
+
+    function showEventLabel(events) {
+        // Get or create the label container
+        let labelContainer = $('#eventLabels');
+        // If container doesn't exist, create it
+        if (labelContainer.length === 0) {
+            $('#calendar').after('<div id="eventLabels"></div>');
+            labelContainer = $('#eventLabels');
+        }
+
+        labelContainer.addClass('card mt-3 p-3');
+        labelContainer.empty();
+
+        // ${''} // ini buat ngambil atau membuat tag baru
+        $('<h5>', {text: 'Events',class: 'mt-3'}).appendTo(labelContainer);
+
+        // ini buat loop label
+        if (events && events.length > 0) {
+            events.forEach(event => {
+                $('<span>', {
+                    text: event.name,
+                    class: 'badge me-2 mt-2',
+                    css: {
+                        fontSize: '14px',
+                        padding: '8px 12px',
+                        backgroundColor: '#1a472a',
+                    }
+                }).appendTo(labelContainer);
+            });
+        } else { // jika ga ada event pasang yang ini
+            $('<p>', {
+                text: 'No events for this date',
+                class: 'text-muted mt-2'
+            }).appendTo(labelContainer);
+        }
+    }
+});
---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,138 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="index.html" class="app-brand-link">
+            <span class="app-brand-logo demo">
+                <!-- Logo bisa ditambahkan di sini -->
+            </span>
+            <span class="app-brand-text demo fw-bolder text-capitalize">EchoPulse</span>
+        </a>
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+    <ul class="menu-inner py-1">
+        <!-- Dashboard -->
+        <li class="menu-item @if (Request::is('/dashboard')) active @endif">
+            <a href="index.html" class="menu-link">
+                {{-- ini make box icons --}}
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+            </a>
+        </li>
+
+        <!-- Master Data -->
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">Master Data</span>
+        </li>
+        <li class="menu-item @if (Request::is('pages/user')) active @endif">
+            <a href="/pages/user"  class="menu-link ">
+                <i class="menu-icon tf-icons bx bx-user"></i>
+                <div data-i18n="User Management">Data Pengguna</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-lock"></i>
+                <div data-i18n="Role Management">Data Peran</div>
+            </a>
+        </li>
+
+        <!-- Form Section -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Formulir</span></li>
+        <li class="menu-item">
+            <a href="cards-basic.html" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-collection"></i>
+                <div data-i18n="Tags">Data Tag</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="cards-basic.html" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-comment-dots"></i>
+                <div data-i18n="Forum Data">Data Forum</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="cards-basic.html" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-chat"></i>
+                <div data-i18n="Comment Data">Data Komentar Forum</div>
+            </a>
+        </li>
+
+        <!-- Proposal Section -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Proposal</span></li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-file"></i>
+                <div data-i18n="Proposal Data">Data Proposal</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-check-circle"></i>
+                <div data-i18n="Proposal Status">Status Proposal</div>
+            </a>
+        </li>
+
+        <!-- Mapping Section -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Pemetaan</span></li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-cog"></i>
+                <div data-i18n="Inventory Data">Inventaris Alat</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-map"></i>
+                <div data-i18n="Mapping Data">Pemetaan Alat</div>
+            </a>
+        </li>
+
+        <!-- Reports Section -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Laporan</span></li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-file-blank"></i>
+                <div data-i18n="Monthly Reports">Laporan Bulanan</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-file-blank"></i>
+                <div data-i18n="Yearly Reports">Laporan Tahunan</div>
+            </a>
+        </li>
+
+        <!-- Settings Section -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Pengaturan</span></li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-user-circle"></i>
+                <div data-i18n="Account Settings">Pengaturan Akun</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-cog"></i>
+                <div data-i18n="System Settings">Pengaturan Sistem</div>
+            </a>
+        </li>
+
+        <!-- Help Section -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Bantuan</span></li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-help-circle"></i>
+                <div data-i18n="Help Center">Pusat Bantuan</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-book-open"></i>
+                <div data-i18n="Documentation">Dokumentasi</div>
+            </a>
+        </li>
+    </ul>
+</aside>
