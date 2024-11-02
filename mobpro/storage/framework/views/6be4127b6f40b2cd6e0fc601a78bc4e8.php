67152f8949dec7441aa164425aae5248b7bd70ea
Rafieaydin <rafie.aydin1@gmail.com>
2024-11-02 18:15:19 +0700

    remove unsused file

--- mobpro/storage/framework/views/6be4127b6f40b2cd6e0fc601a78bc4e8.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\LOQ\Documents\Sekolah\Semester 5\Perancangan dan pemograman web\tubes\mobpro\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
