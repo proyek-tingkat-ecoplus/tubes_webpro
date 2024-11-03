6ee76a4623f214e0a905342ca74790b1885e9231
nasyaarnm <nasyaarnm@student.telkomuniversity.ac.id>
2024-11-03 20:22:07 +0700

    perbaikan card dan routes

--- resources/views/profilKepdes.blade.php
+++ resources/views/profilKepdes.blade.php
@@ -1,4 +1,7 @@
 @extends('main.main')
+@push('styles')
+<link rel="stylesheet" href="{{asset('asset/css/pages/profil_kepdes.css')}}">
+@endpush
 @section('content')
 <div class="container-fluid">
     <img src="{{asset('asset/image/pabrik.jpeg')}}" class="img-fluid img-hero" alt="Responsive image">
@@ -7,23 +10,23 @@
         <div class="profile-container">
             <div class="profile-card">
                 <img src="{{asset('asset/image/orang.png')}}" class="img-fluid rounded-circle mb-3" alt="Kepala Dinas">
-                <h5 class="fw-bold text-white">Dr. Rendy Nugraha, S.T., M.T., Ph.D.</h5>
-                <p><br>Kepala Dinas Energi Bersih Terbarukan</p>
+                <h5 class="fw-bold text-white">Dr. Rendy, S.T., M.T., Ph.D.</h5>
+                <div class="text-light mt-2">Kepala Dinas Energi Bersih Terbarukan</div>
             </div>
             <div class="profile-card">
                 <img src="{{asset('asset/image/orang.png')}}" class="img-fluid rounded-circle mb-3" alt="Wakil Kepala Dinas">
-                <h5 class="fw-bold text-green">Dr. Ratna Kusuma, S.T., M.T., Ph.D.</h5>
-                <p><br>Wakil Kepala Dinas Energi Bersih Terbarukan</p>
+                <h5 class="fw-bold text-green">Dr. Ratna , S.T., M.T., Ph.D.</h5>
+                <div class="text-green mt-2">Wakil Kepala Dinas Energi Bersih Terbarukan</div>
             </div>
             <div class="profile-card">
                 <img src="{{asset('asset/image/orang.png')}}" class="img-fluid rounded-circle mb-3" alt="Sekretaris Dinas">
-                <h5 class="fw-bold text-white">Dr. Arif Pratama, S.T., M.T., Ph.D.</h5>
-                <p><br>Sekretaris Dinas Energi Bersih Terbarukan</p>
+                <h5 class="fw-bold text-white">Dr. Arif , S.T., M.T., Ph.D.</h5>
+                <div class="text-light mt-2">Sekretaris Dinas Energi Bersih Terbarukan</div>
             </div>
             <div class="profile-card">
                 <img src="{{asset('asset/image/orang.png')}}" class="img-fluid rounded-circle mb-3" alt="Bendahara Dinas">
-                <h5 class="fw-bold text-green">Dr. Diandra, S.T., M.T., Ph.D.</h5>
-                <p><br>Bendahara Dinas Energi Bersih Terbarukan</p>
+                <h5 class="fw-bold text-green ">Dr. josep, S.T., M.T., Ph.D.</h5>
+                <div class="text-green mt-2">Bendahara Dinas Energi Bersih Terbarukan</div>
             </div>
         </div>
     </div>
