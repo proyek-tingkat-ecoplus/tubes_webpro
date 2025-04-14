d63c3b5ea4572b95cd19b6631c6fd71fbf4e2527
nasyaarnm <nasyaarnm@student.telkomuniversity.ac.id>
2025-04-14 10:14:03 +0700

    dark mode

--- resources/views/user/master/navbar.blade.php
+++ resources/views/user/master/navbar.blade.php
@@ -1,11 +1,12 @@
 <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light m-5  shadow-lg">
     <div class="container-fluid">
-        <a class="navbar-brand m-md-3 " href="#">EchoPulse</a>
+        <a class="navbar-brand m-md-3 " href="#">EcoPulse</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <button id="darkModeToggle" class="btn btn-dark">🌙</button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             {{-- Nav item --}}
             <ul class="navbar-nav text-center me-auto ms-auto mb-2 mb-lg-0">
